a0f7d0017608dcd7147c7d3002ad22f194763b5b
vinith579 <vinithvinith356140@gmail.com>
2024-08-03 00:50:03 +0530

    small update

--- stript.js
+++ stript.js
@@ -5,6 +5,5 @@
     toggleBtn.onclick = function () {
       dropDownMenu.classList.toggle('open');
       const isOpen = dropDownMenu.classList.contains('open');
-
       toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
     }
